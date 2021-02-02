35b02acc2864227311848531b99fd7693f88c2be
arita37 <noelkev0@gmail.com>
2021-02-02 16:37:36 +0900

    cleanup
    Former-commit-id: e8912a6f578a09050979ad0ab8a303a3d2d9b0be
    Former-commit-id: 35e5f28821e08e1cf4a4e3514c6c838622f79ff8

--- example/tseries/tseries_sales.py
+++ example/tseries/tseries_sales.py
@@ -39,59 +39,6 @@ def pd_merge(df_list, cols_join):
         dfall      = dfall.join(dfi.set_index(cols_joini), on = cols_joini, how="left") if dfall is not None else dfi
     return dfall
 
-
-def train(input_path, n_experiments = 3, colid = None, coly = None):
-    """
-       Generic train
-    :param input_path:
-    :param max_rows:
-    :param n_experiments:
-    :param colid:
-    :param coly:
-    :return:
-    """
-    model_params = {'num_leaves': 555,
-          'min_child_weight' : 0.034, 'feature_fraction' : 0.379, 'bagging_fraction' : 0.418, 'min_data_in_leaf' : 106, 'objective'        : 'regression', 'max_depth'        : -1, 'learning_rate'    : 0.005, "boosting_type"    : "gbdt", "bagging_seed"     : 11, "metric"           : 'rmse', "verbosity"        : -1, 'reg_alpha'        : 0.3899, 'reg_lambda'       : 0.648, 'random_state'     : 222,
-         }
-
-    #coly = 'demand'
-
-    dict_metrics = {'run_id' : [], 'cols' : [], 'metric_name': [], 'model_params': [], 'metrics_val' : []}
-    for ii in range(n_experiments):
-        colsX  = featurestore_filter_features(colid = colid, coly = coly)
-        df     = featurestore_get_feature_fromcolname(input_path, colsX + [coly], colid)
-
-        X 	               = df[ colsX ]
-        y            	   = df[coly]
-
-        ############## ALready in DSA 2 ##########################################################
-        X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-        # Y_test = np.zeros(X_test.shape[0])
-
-        dtrain                     = lgb.Dataset(X_train, label=Y_train)
-        dtest                      = lgb.Dataset(X_test, label=Y_test)
-        clf                        = lgb.train(model_params, dtrain, 2500,
-                                               valid_sets = [dtrain, dtest],
-                                               early_stopping_rounds = 50, verbose_eval=100)
-
-        Y_test_pred          	   = clf.predict(X_test,num_iteration=clf.best_iteration)
-        val_score                  = np.sqrt(metrics.mean_squared_error(Y_test_pred, Y_test))
-
-        #### Metrics  ####################################################
-        dict_metrics['run_id'].append(datetime.now())
-        dict_metrics['cols'].append(";".join(X_train.columns.tolist()))
-        dict_metrics['model_params'].append(model_params)
-        dict_metrics['metric_name'].append('rmse')
-        dict_metrics['metrics_val'].append(val_score)
-
-    df_metrics = pd.DataFrame.from_dict(dict_metrics)
-    print("        DF metrics          ")
-    print(df_metrics)
-    df_metrics.to_csv("df_metrics.csv")
-
-
-
-
 ########################################################################################################################
 ########################################################################################################################
 class FeatureStore(object):
@@ -208,10 +155,6 @@ def featurestore_get_feature_fromcolname(path, selected_cols, colid):
 
 ########################################################################################################################
 ########################################################################################################################
-
-
-
-
 def custom_get_colsname(colid, coly):
     coldrop = colid + [coly]
     meta_csv = pd.read_csv('meta_features.csv')
@@ -272,7 +215,7 @@ def custom_generate_feature_all(input_path = data_path, out_path=".", input_raw_
 
 
 
-def run_train(input_path ="data/input/tseries/retail/raw", out_path=data_path,
+def run_generate_train_data(input_path ="data/input/tseries/retail/raw", out_path=data_path,
               do_generate_raw=True, do_generate_feature=True, do_train=False,
               max_rows = 10):
 
@@ -281,7 +224,7 @@ def run_train(input_path ="data/input/tseries/retail/raw", out_path=data_path,
 
 
     if do_generate_feature :
-      custom_generate_feature_all(input_path="data/output", out_path="",
+      custom_generate_feature_all(input_path="../../data/output", out_path="",
                                   input_raw_path =input_path + "/sales-data-set.csv",
                                   auxiliary_csv_path =input_path + "/calendar_gen.csv",
 
@@ -294,225 +237,10 @@ def run_train(input_path ="data/input/tseries/retail/raw", out_path=data_path,
                                   max_rows  = max_rows,
                                   coldate = "Date")
 
-    if do_train :
-        train(input_path="data/output", colid = ["date", "item_id_col"], coly ="demand")
-
-
 if __name__ == "__main__":
-    run_train()
+    run_generate_train_data()
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-"""
-
-
-import util_feat_m5
-
-
-
-# df_meta=   col_name, col_type, file_path
-
-
-
-def featurestore_generate_feature(dir_in, dir_out, my_fun_features) :
-
-    # from util_feat_m5  import lag_featrues
-    # featurestore_generate_feature(dir_in, dir_out, lag_featrues)
-
-    train_df = pd.read_csv( dir_in  + "/sales_train_val.csv.zip")
-    calendar_df = pd.read_csv(dir_in  + "/calendar.csv")
-    price_df = pd.read_csv(dir_in  + "/sell_prices.csv")
-
-
-    dfnew = my_fun_features(train_df, calendar_df, price_df) :
-
-    dfnew.to_parquet( dir_out +"/mfeaturesXXXX.parquet")
-
-
-
- def featurestore_filter_features(mode="random") :
-    cols_cat0 = [  "feat1", "fewat2" ]
-
-    if mode == "random" :
-        ### Random selection
-        cols_cat = cols_cat0[ np.random.c = hoice( 5, len(cols_cat)  ) ]
-        cols_num = cols_num0[ np.random.c = hoice( 5, len(cols_num)  ) ]
-        return cols_cat, col_num
-
-    if mode == "all" :
-      pass
-
-    if mode == "smartway" :
-       pass
-
-
-
-def train(model, pars={} ) :
-
-    data_pars = {}
-    model_pars = {}
-
-    for ii in range(n_experiments) :
-        cols_cat, cols_num = featurestore_filter_features()
-
-        df     = featurestore_get_feature_fromcolname(path, cols_cat + cols_num, "train")
-        dftest = featurestore_get_feature_fromcolname(path, cols_cat + cols_num, 'test')
-
-        X_train = X_transform( df, cols_num, cols_cat, pars) # select sri
-        y_train  = y_transform(df, coly)
-
-        X_test = X_transform( dftest, cols_num, cols_cat, pars) # select variables
-        y_test  = y_transform(dftest, coly)
-
-
-
-        lgbm = lgb.LGBMRegressor()
-        lgbm.fit( X_train, y_train)
-
-        # prediction + metrics
-        y_test_pred = lgbm.predict(X_test)
-        metric_val = metrics_calc(y_test, y_test_pred)
-
-
-        ### Store in metrics :
-
-        # run_id, feat_name, feat_name_long, feat_type, model_params, metric_name, metric_val
-        # 3,roling_demand,Mean of the variable estimates,lag_features,params = {"objective" : "poisson","metric" :"rmse","force_row_wise" : True,"learning_rate" : 0.075,
-    "sub_row" : 0.75,"bagging_freq" : 1,"lambda_l2" : 0.1,"metric": ["rmse"],'verbosity': 1,'num_iterations' : 250,
-    },rmse,1.16548
-
-        df_metrics['run_id'] = time()
-        df_metrics['cols'].append(  ",".join( cols_num + cols_cat ))
-        df_metrics['metrics_val'].append(metric_val)
-
-
-
-
-
-def test_old():
-    from util_feat_m5  import lag_featrues
-    train_df = pd.read_csv("sales_train_val.csv")
-    calendar_df = pd.read_csv("calendar.csv")
-    price_df = pd.read_csv("sell_prices.csv")
-    sample = pd.read_csv("sample_submi.csv")
-    calendar_df["date_dt"] = pd.to_datetime(calendar_df["date"])
-    train  = train_df.copy()
-    price = price_df.copy()
-    calendar = calendar_df.copy()
-    Train_data = train.iloc[:,:-56]
-    Val_data = train.iloc[:,:-28]
-
-    X_train = lag_featrues(Train_data).iloc[:,5:] # select variables
-    y_train = train.iloc[:,-56]
-    X_test = lag_featrues(Val_data).iloc[:,5:]
-    y_test = train.iloc[:,-28]
-
-    # Create instance
-    lgbm = lgb.LGBMRegressor()
-
-    # Training and score
-    learning_rate = [0.15, 0.2, 0.25]
-    max_depth = [15, 20, 25]
-
-    param_grid = {'learning_rate': learning_rate, 'max_depth': max_depth}
-
-    # Fitting
-    cv_lgbm = GridSearchCV(lgbm, param_grid, cv=10, n_jobs =1)
-    cv_lgbm.fit(X_train, y_train)
-
-    print("Best params:{}".format(cv_lgbm.best_params_))
-
-    # best params
-    best_lg = cv_lgbm.best_estimator_
-
-    # prediction
-    y_train_pred_lg = best_lg.predict(X_train)
-    y_test_pred_lg = best_lg.predict(X_test)
-
-    print("MSE train:{}".format(mean_squared_error(y_train, y_train_pred_lg)))
-    print("MSE test;{}".format(mean_squared_error(y_test, y_test_pred_lg)))
-
-    print("R2 score train:{}".format(r2_score(y_train, y_train_pred_lg)))
-    print("R2 score test:{}".format(r2_score(y_test, y_test_pred_lg)))
-    #Predict using only variables with an importance of 1 or higher.
-    importance = best_lg.feature_importances_
-
-    indices = np.argsort(importance)[::-1]
-
-    # print importance
-    importance_df = pd.DataFrame({})
-    columns = []
-    importance_ = []
-    for f in range(X_train.shape[1]):
-        print("%2d) %-*s %.2f" %(f+1, 30, X_train.columns[indices[f]], importance[indices[f]]))
-        col = X_train.columns[indices[f]]
-        imp = importance[indices[f]]
-        columns.append(col)
-        importance_.append(imp)
-    importance_df["col_name"] = columns
-    importance_df["importance"] = importance_
-    importance = best_lg.feature_importances_
-
-    indices = np.argsort(importance)[::-1]
-
-    # importance columns (>0)
-    imp_col = importance_df[importance_df["importance"]>0]["col_name"].values
-
-    # Train test split, select by imp_col
-
-    X_train = lag_featrues(Train_data).iloc[:,5:][imp_col] # select variables
-    y_train = train.iloc[:,-56]
-    X_test = lag_featrues(Val_data).iloc[:,5:][imp_col]
-    y_test = train.iloc[:,-28]
-
-    # Create instance
-    lgbm = lgb.LGBMRegressor()
-
-    # Training and score
-    learning_rate = [0.15, 0.2, 0.25]
-    max_depth = [15, 20, 25]
-
-    param_grid = {'learning_rate': learning_rate, 'max_depth': max_depth}
-
-    # Fitting
-    cv_lgbm = GridSearchCV(lgbm, param_grid, cv=10, n_jobs =1)
-    cv_lgbm.fit(X_train, y_train)
-
-    print("Best params:{}".format(cv_lgbm.best_params_))
-
-    # best params
-    best_lg = cv_lgbm.best_estimator_
-
-    # prediction
-    y_train_pred_lg = best_lg.predict(X_train)
-    y_test_pred_lg = best_lg.predict(X_test)
-
-    print("MSE train:{}".format(mean_squared_error(y_train, y_train_pred_lg)))
-    print("MSE test;{}".format(mean_squared_error(y_test, y_test_pred_lg)))
-
-    print("R2 score train:{}".format(r2_score(y_train, y_train_pred_lg)))
-    print("R2 score test:{}".format(r2_score(y_test, y_test_pred_lg)))
-    run_id=list(range(300))
-    df_metrics=pd.DataFrame(run_id,columns=['run_id'])
-    df_metrics['feat_name'] = pd.Series(X_train.columns[0:300], index=dataframe.index)
-    df_metrics['feat_type']=df_metrics.feat_name
-    df_metrics.replace({'feat_type': r'^lag_.*'}, {'feat_type': 'lag'}, regex=True,inplace=True)
-    df_metrics.replace({'feat_type': r'^rolling.*'}, {'feat_type': 'rolling'}, regex=True,inplace=True)
-    df_metrics['parameter'] = pd.Series(best_lg, index=dataframe.index)
-    df_metrics['metric_name'] ="MSE"
-    df_metrics['metric_val'] = pd.Series(pred_mse[:300], index=dataframe.index)
-    df_metrics.to_csv("train.csv")
-"""
