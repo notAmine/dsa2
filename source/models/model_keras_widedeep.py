e7ab3221434c389b2118e872db449199c761a191
arita37 <noelkev0@gmail.com>
2021-02-04 16:06:47 +0900

    pytorch
    Former-commit-id: 6f6ce98f93dd29e1d545e52fa6d095a7acf847fe
    Former-commit-id: 272f68157071667a763ed21abd9fca382c19f79c

--- source/models/model_keras_widedeep.py
+++ source/models/model_keras_widedeep.py
@@ -2,6 +2,11 @@
 # -*- coding: utf-8 -*-
 """
 python source/models/model_keras_widedeep.py
+
+
+pip install Keras==2.4.3
+
+
 """
 import os
 import pandas as pd, numpy as np, scipy as sci
@@ -272,8 +277,8 @@ def get_params(param_pars={}, **kw):
 def test(config=''):
     global model, session
 
-    X = np.random.rand(10000,30)
-    y = np.random.binomial(n=1, p=0.5, size=[10000])
+    X = np.random.rand(100,30)
+    y = np.random.binomial(n=1, p=0.5, size=[100])
 
     X_train_full, X_test, y_train_full, y_test = train_test_split(X, y, random_state=2021, stratify=y)
     X_train, X_valid, y_train, y_valid = train_test_split(X_train_full, y_train_full, random_state=2021, stratify=y_train_full)
@@ -301,7 +306,7 @@ def test(config=''):
                  'n_wide_features': n_wide_features,
                  'n_deep_features': n_deep_features,
                 }
-    compute_pars = { 'compute_pars' : { 'epochs': 50,
+    compute_pars = { 'compute_pars' : { 'epochs': 2,
                     'callbacks': callbacks} }
 
     model = Model(model_pars=model_pars, data_pars=data_pars, compute_pars=compute_pars)
