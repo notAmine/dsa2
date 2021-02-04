bcef17a024e82f45a929b2fbd7f73f85dab93930
arita37 <noelkev0@gmail.com>
2021-02-04 13:53:25 +0900

    models/model_torch_tabular.py
    Former-commit-id: c56ac3e527535ea03db97dd7b25d9add4e52a48f
    Former-commit-id: 2e3a1f331ca987faeea627ded5f98b7d38985441

--- source/models/model_torch_tabular.py
+++ source/models/model_torch_tabular.py
@@ -50,16 +50,52 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 
-from torch import nn
 import torch
+from torch import nn
 
 
-try
+try :
     from pytorch_tabular import TabularModel
     from pytorch_tabular.models import CategoryEmbeddingModelConfig
     from pytorch_tabular.config import DataConfig, OptimizerConfig, TrainerConfig, ExperimentConfig
 except :
     os.system("pip install pytorch_tabular[all]")
+
+
+import torch
+import numpy as np
+from torch.functional import norm
+# torch.manual_seed(0)
+# np.random.seed(0)
+# torch.set_deterministic(True)
+
+from sklearn.datasets import fetch_covtype
+
+# from torch.utils import data
+from pytorch_tabular.config import (
+    DataConfig,
+    ExperimentConfig,
+    ExperimentRunManager,
+    ModelConfig,
+    OptimizerConfig,
+    TrainerConfig,
+)
+from pytorch_tabular.models.node.config import NodeConfig
+from pytorch_tabular.models.category_embedding.config import (
+    CategoryEmbeddingModelConfig,
+)
+from pytorch_tabular.models.category_embedding.category_embedding_model import (
+    CategoryEmbeddingModel,
+)
+import pandas as pd
+from omegaconf import OmegaConf
+from pytorch_tabular.tabular_datamodule import TabularDatamodule
+from pytorch_tabular.tabular_model import TabularModel
+import pytorch_lightning as pl
+from sklearn.preprocessing import PowerTransformer
+from sklearn.model_selection import train_test_split
+from pathlib import Path
+import wget
 
 
 ####################################################################################################
@@ -153,9 +189,8 @@ class Model(object):
               categorical_cols = dm['colcat'],
             )
 
-
-            model_config   = CategoryEmbeddingModelConfig( **model_pars['model_pars']    )
-            trainer_config = TrainerConfig( **compute_pars['compute_pars'] )
+            model_config     = CategoryEmbeddingModelConfig( **model_pars['model_pars']    )
+            trainer_config   = TrainerConfig( **compute_pars['compute_pars'] )
             optimizer_config = OptimizerConfig()
 
             self.config_pars = { 'data_config' : data_config,
@@ -178,10 +213,14 @@ def fit(data_pars=None, compute_pars=None, out_pars=None, **kw):
     session = None  # Session type for compute
     Xtrain, ytrain, Xtest, ytest = get_dataset(data_pars, task_type="train")
 
-    Xtrain = torch.tensor(Xtrain.values, dtype=torch.float)
-    Xtest  = torch.tensor(Xtest.values, dtype=torch.float)
-    ytrain = torch.tensor(ytrain.values, dtype=torch.float)
-    ytest  = torch.tensor(ytest.values, dtype=torch.float)
+    #Xtrain = torch.tensor(Xtrain.values, dtype=torch.float)
+    #Xtest  = torch.tensor(Xtest.values, dtype=torch.float)
+    #ytrain = torch.tensor(ytrain.values, dtype=torch.float)
+    #ytest  = torch.tensor(ytest.values, dtype=torch.float)
+
+    train = pd.concat((Xtrain,ytest)).values
+    val   = pd.concat((Xtrain,ytest)).values
+
 
     if VERBOSE: log(Xtrain, model.model)
 
@@ -189,15 +228,13 @@ def fit(data_pars=None, compute_pars=None, out_pars=None, **kw):
     compute_pars2 = compute_pars.get('compute_pars', {})
 
 
-
+    ypred =  model.model.fittrain=train, validation=val)
 
 
 
 
     #############################################################
-    df_loss = pd.DataFrame(losses)
-    df_loss['loss'].plot()
-    return df_loss
+    return ypred
 
 
 def predict(Xpred=None, data_pars={}, compute_pars=None, out_pars={}, **kw):
@@ -372,8 +409,101 @@ def test(config=''):
     """
     global model, session
 
-    X = np.random.rand(10000,20)
-    y = np.random.binomial(n=1, p=0.5, size=[10000])
+    #X = np.random.rand(10000,20)
+    #y = np.random.binomial(n=1, p=0.5, size=[10000])
+
+    BASE_DIR = Path.home().joinpath('data/input/covtype/')
+    datafile = BASE_DIR.joinpath('covtype.data.gz')
+    datafile.parent.mkdir(parents=True, exist_ok=True)
+    url = "https://archive.ics.uci.edu/ml/machine-learning-databases/covtype/covtype.data.gz"
+    if not datafile.exists():
+        wget.download(url, datafile.as_posix())
+
+    target_name = ["Covertype"]
+
+    cat_col_names = [ "Wilderness_Area1", "Wilderness_Area2", "Wilderness_Area3", "Wilderness_Area4", "Soil_Type1", "Soil_Type2", "Soil_Type3", "Soil_Type4", "Soil_Type5", "Soil_Type6", "Soil_Type7", "Soil_Type8", "Soil_Type9", "Soil_Type10", "Soil_Type11", "Soil_Type12", "Soil_Type13", "Soil_Type14", "Soil_Type15", "Soil_Type16", "Soil_Type17", "Soil_Type18", "Soil_Type19", "Soil_Type20", "Soil_Type21", "Soil_Type22", "Soil_Type23", "Soil_Type24", "Soil_Type25", "Soil_Type26", "Soil_Type27", "Soil_Type28", "Soil_Type29", "Soil_Type30", "Soil_Type31", "Soil_Type32", "Soil_Type33", "Soil_Type34", "Soil_Type35", "Soil_Type36", "Soil_Type37", "Soil_Type38", "Soil_Type39", "Soil_Type40"
+                      ]
+
+    num_col_names = [ "Elevation", "Aspect", "Slope", "Horizontal_Distance_To_Hydrology", "Vertical_Distance_To_Hydrology", "Horizontal_Distance_To_Roadways", "Hillshade_9am", "Hillshade_Noon", "Hillshade_3pm", "Horizontal_Distance_To_Fire_Points"
+    ]
+
+    feature_columns = (
+        num_col_names + cat_col_names + target_name)
+
+    df = pd.read_csv(datafile, header=None, names=feature_columns)
+    # cat_col_names = []
+
+    # num_col_names = [
+    #     "Elevation", "Aspect"
+    # ]
+    # feature_columns = (
+    #     num_col_names + cat_col_names + target_name)
+    # df = df.loc[:,feature_columns]
+    df.head()
+    train, test = train_test_split(df, random_state=42)
+    train, val = train_test_split(train, random_state=42)
+    num_classes = len(set(train[target_name].values.ravel()))
+
+
+    data_config = DataConfig(
+        target=target_name,
+        continuous_cols=num_col_names,
+        categorical_cols=cat_col_names,
+        continuous_feature_transform=None,#"quantile_normal",
+        normalize_continuous_features=False
+    )
+    model_config = CategoryEmbeddingModelConfig(task="classification",
+                                                metrics=["f1","accuracy"],
+                                                metrics_params=[{"num_classes":num_classes},{}])
+    # model_config = NodeConfig(
+    #     task="classification",
+    #     depth=4,
+    #     num_trees=1024,
+    #     input_dropout=0.0,
+    #     metrics=["f1", "accuracy"],
+    #     metrics_params=[{"num_classes": num_classes, "average": "macro"}, {}],
+    # )
+    trainer_config = TrainerConfig(gpus=1, fast_dev_run=True)
+    experiment_config = ExperimentConfig(project_name="PyTorch Tabular Example",
+                                         run_name="node_forest_cov",
+                                         exp_watch="gradients",
+                                         log_target="wandb",
+                                         log_logits=True)
+    optimizer_config = OptimizerConfig()
+
+    # tabular_model = TabularModel(
+    #     data_config="examples/data_config.yml",
+    #     model_config="examples/model_config.yml",
+    #     optimizer_config="examples/optimizer_config.yml",
+    #     trainer_config="examples/trainer_config.yml",
+    #     # experiment_config=experiment_config,
+    # )
+    tabular_model = TabularModel(
+        data_config=data_config,
+        model_config=model_config,
+        optimizer_config=optimizer_config,
+        trainer_config=trainer_config,
+        # experiment_config=experiment_config,
+    )
+    tabular_model.fit(
+        train=train, validation=val)
+
+    result = tabular_model.evaluate(test)
+    print(result)
+    test.drop(columns=target_name, inplace=True)
+    pred_df = tabular_model.predict(test)
+    pred_df.to_csv("output/temp2.csv")
+    # tabular_model.save_model("test_save")
+    # new_model = TabularModel.load_from_checkpoint("test_save")
+    # result = new_model.evaluate(test)
+
+
+
+
+
+
+
+
 
     X_train_full, X_test, y_train_full, y_test = train_test_split(X, y, random_state=2021, stratify=y)
     X_train, X_valid, y_train, y_valid         = train_test_split(X_train_full, y_train_full, random_state=2021, stratify=y_train_full)
@@ -431,3 +561,139 @@ def test(config=''):
 if __name__ == "__main__":
     import fire
     fire.Fire(test)
+
+
+"""
+import torch
+import numpy as np
+from torch.functional import norm
+# torch.manual_seed(0)
+# np.random.seed(0)
+# torch.set_deterministic(True)
+
+from sklearn.datasets import fetch_covtype
+
+# from torch.utils import data
+from pytorch_tabular.config import (
+    DataConfig,
+    ExperimentConfig,
+    ExperimentRunManager,
+    ModelConfig,
+    OptimizerConfig,
+    TrainerConfig,
+)
+from pytorch_tabular.models.node.config import NodeConfig
+from pytorch_tabular.models.category_embedding.config import (
+    CategoryEmbeddingModelConfig,
+)
+from pytorch_tabular.models.category_embedding.category_embedding_model import (
+    CategoryEmbeddingModel,
+)
+import pandas as pd
+from omegaconf import OmegaConf
+from pytorch_tabular.tabular_datamodule import TabularDatamodule
+from pytorch_tabular.tabular_model import TabularModel
+import pytorch_lightning as pl
+from sklearn.preprocessing import PowerTransformer
+from sklearn.model_selection import train_test_split
+from pathlib import Path
+import wget
+
+
+BASE_DIR = Path.home().joinpath('data')
+datafile = BASE_DIR.joinpath('covtype.data.gz')
+datafile.parent.mkdir(parents=True, exist_ok=True)
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/covtype/covtype.data.gz"
+if not datafile.exists():
+    wget.download(url, datafile.as_posix())
+
+target_name = ["Covertype"]
+
+cat_col_names = [
+    "Wilderness_Area1", "Wilderness_Area2", "Wilderness_Area3",
+    "Wilderness_Area4", "Soil_Type1", "Soil_Type2", "Soil_Type3", "Soil_Type4",
+    "Soil_Type5", "Soil_Type6", "Soil_Type7", "Soil_Type8", "Soil_Type9",
+    "Soil_Type10", "Soil_Type11", "Soil_Type12", "Soil_Type13", "Soil_Type14",
+    "Soil_Type15", "Soil_Type16", "Soil_Type17", "Soil_Type18", "Soil_Type19",
+    "Soil_Type20", "Soil_Type21", "Soil_Type22", "Soil_Type23", "Soil_Type24",
+    "Soil_Type25", "Soil_Type26", "Soil_Type27", "Soil_Type28", "Soil_Type29",
+    "Soil_Type30", "Soil_Type31", "Soil_Type32", "Soil_Type33", "Soil_Type34",
+    "Soil_Type35", "Soil_Type36", "Soil_Type37", "Soil_Type38", "Soil_Type39",
+    "Soil_Type40"
+]
+
+num_col_names = [
+    "Elevation", "Aspect", "Slope", "Horizontal_Distance_To_Hydrology",
+    "Vertical_Distance_To_Hydrology", "Horizontal_Distance_To_Roadways",
+    "Hillshade_9am", "Hillshade_Noon", "Hillshade_3pm",
+    "Horizontal_Distance_To_Fire_Points"
+]
+
+feature_columns = (
+    num_col_names + cat_col_names + target_name)
+
+df = pd.read_csv(datafile, header=None, names=feature_columns)
+# cat_col_names = []
+
+# num_col_names = [
+#     "Elevation", "Aspect"
+# ]
+# feature_columns = (
+#     num_col_names + cat_col_names + target_name)
+# df = df.loc[:,feature_columns]
+df.head()
+train, test = train_test_split(df, random_state=42)
+train, val = train_test_split(train, random_state=42)
+num_classes = len(set(train[target_name].values.ravel()))
+
+data_config = DataConfig(
+    target=target_name,
+    continuous_cols=num_col_names,
+    categorical_cols=cat_col_names,
+    continuous_feature_transform=None,#"quantile_normal",
+    normalize_continuous_features=False
+)
+model_config = CategoryEmbeddingModelConfig(task="classification", metrics=["f1","accuracy"], metrics_params=[{"num_classes":num_classes},{}])
+# model_config = NodeConfig(
+#     task="classification",
+#     depth=4,
+#     num_trees=1024,
+#     input_dropout=0.0,
+#     metrics=["f1", "accuracy"],
+#     metrics_params=[{"num_classes": num_classes, "average": "macro"}, {}],
+# )
+trainer_config = TrainerConfig(gpus=1, fast_dev_run=True)
+experiment_config = ExperimentConfig(project_name="PyTorch Tabular Example", 
+                                     run_name="node_forest_cov", 
+                                     exp_watch="gradients", 
+                                     log_target="wandb", 
+                                     log_logits=True)
+optimizer_config = OptimizerConfig()
+
+# tabular_model = TabularModel(
+#     data_config="examples/data_config.yml",
+#     model_config="examples/model_config.yml",
+#     optimizer_config="examples/optimizer_config.yml",
+#     trainer_config="examples/trainer_config.yml",
+#     # experiment_config=experiment_config,
+# )
+tabular_model = TabularModel(
+    data_config=data_config,
+    model_config=model_config,
+    optimizer_config=optimizer_config,
+    trainer_config=trainer_config,
+    # experiment_config=experiment_config,
+)
+tabular_model.fit(
+    train=train, validation=val)
+
+result = tabular_model.evaluate(test)
+print(result)
+test.drop(columns=target_name, inplace=True)
+pred_df = tabular_model.predict(test)
+pred_df.to_csv("output/temp2.csv")
+# tabular_model.save_model("test_save")
+# new_model = TabularModel.load_from_checkpoint("test_save")
+# result = new_model.evaluate(test)
+
+"""
