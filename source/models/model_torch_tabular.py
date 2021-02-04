0a8d1669c13c43c0fecf52ca35f802db7008cd33
arita37 <noelkev0@gmail.com>
2021-02-04 14:20:01 +0900

    source/models/model_torch_tabular.py
    Former-commit-id: 6df7c9c7ebae262ed9407993df92121480af268b
    Former-commit-id: 4d9fd5dae25994a928ae37573a003dd1eb2f47b1

--- source/models/model_torch_tabular.py
+++ source/models/model_torch_tabular.py
@@ -1,45 +1,7 @@
 # pylint: disable=C0321,C0103,C0301,E1305,E1121,C0302,C0330,C0111,W0613,W0611,R1705
 # -*- coding: utf-8 -*-
 """
-
-
 https://github.com/arita37/pytorch_tabular
-from pytorch_tabular import TabularModel
-from pytorch_tabular.models import CategoryEmbeddingModelConfig
-from pytorch_tabular.config import DataConfig, OptimizerConfig, TrainerConfig, ExperimentConfig
-
-data_config = DataConfig(
-    target=['target'], #target should always be a list. Multi-targets are only supported for regression. Multi-Task Classification is not implemented
-    continuous_cols=num_col_names,
-    categorical_cols=cat_col_names,
-)
-trainer_config = TrainerConfig(
-    auto_lr_find=True, # Runs the LRFinder to automatically derive a learning rate
-    batch_size=1024,
-    max_epochs=100,
-    gpus=1, #index of the GPU to use. 0, means CPU
-)
-optimizer_config = OptimizerConfig()
-
-model_config = CategoryEmbeddingModelConfig(
-    task="classification",
-    layers="1024-512-512",  # Number of nodes in each layer
-    activation="LeakyReLU", # Activation between each layers
-    learning_rate = 1e-3
-)
-
-tabular_model = TabularModel(
-    data_config=data_config,
-    model_config=model_config,
-    optimizer_config=optimizer_config,
-    trainer_config=trainer_config,
-)
-tabular_model.fit(train=train, validation=val)
-result = tabular_model.evaluate(test)
-pred_df = tabular_model.predict(test)
-tabular_model.save_model("examples/basic")
-loaded_model = TabularModel.load_from_checkpoint("examples/basic")
-
 
 
         The core model which orchestrates everything from initializing the datamodule, the model, trainer, etc.
@@ -79,14 +41,8 @@ from sklearn.model_selection import train_test_split
 from pathlib import Path
 import wget
 
-
 ####################################################################################################
 VERBOSE = False
-
-
-
-# MODEL_URI = get_model_uri(__file__)
-
 
 def log(*s):
     print(*s, flush=True)
@@ -94,7 +50,6 @@ def log(*s):
 
 ####################################################################################################
 global model, session
-
 
 def init(*kw, **kwargs):
     global model, session
