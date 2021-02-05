183e75aa0209600b27c7bfb1eec5c6227305852c
arita37 <noelkev0@gmail.com>
2021-02-05 15:38:17 +0900

    run_bentoml.py
    Former-commit-id: d9e4654d360d68563de3e96023af8f6879fa1d99
    Former-commit-id: 8b2e633860bcd5c43be41d3af7dee27245ef1f8c

--- source/run_bentoml.py
+++ source/run_bentoml.py
@@ -110,6 +110,83 @@ dir_model        = root + "/data/output/titanic/titanic_lightgbm/model/"
 model_uri        = root + "/titanic_classifier.py::titanic_lightgbm"
 
 
+
+import os
+from bentoml.exceptions import MissingDependencyException
+from bentoml.service.artifacts import BentoServiceArtifact
+
+
+
+class dsaModelArtifact(BentoServiceArtifact):
+    """
+    Abstraction for saving/loading scikit learn models using sklearn.externals.joblib
+
+    Args:
+        name (str): Name for the artifact
+        pickle_extension (str): The extension format for pickled file
+
+    Raises:
+        MissingDependencyException: sklean package is required for SklearnModelArtifact
+
+    Example usage:
+
+    >>> from sklearn import svm
+    >>>
+    >>> model_to_save = svm.SVC(gamma='scale')
+    >>> # ... training model, etc.
+    >>>
+    >>> import bentoml
+    >>> from bentoml.frameworks.sklearn import SklearnModelArtifact
+    >>> from bentoml.adapters import DataframeInput
+    >>>
+    >>> @bentoml.env(infer_pip_packages=True)
+    >>> @bentoml.artifacts([SklearnModelArtifact('model')])
+    >>> class SklearnModelService(bentoml.BentoService):
+    >>>
+    >>>     @bentoml.api(input=DataframeInput(), batch=True)
+    >>>     def predict(self, df):
+    >>>         result = self.artifacts.model.predict(df)
+    >>>         return result
+    >>>
+    >>> svc = SklearnModelService()
+    >>>
+    >>> # Pack directly with sklearn model object
+    >>> svc.pack('model', model_to_save)
+    """
+
+    def __init__(self, name, pickle_extension=".pkl"):
+        super(SklearnModelArtifact, self).__init__(name)
+
+        self._pickle_extension = pickle_extension
+        self._model = None
+
+    def _model_file_path(self, base_path):
+        return os.path.join(base_path, self.name + self._pickle_extension)
+
+    def pack(self, sklearn_model, metadata=None):  # pylint:disable=arguments-differ
+        self._model = sklearn_model
+        return self
+
+    def load(self, path):
+        """
+         
+        """
+        model_file_path = self._model_file_path(path)
+        sklearn_model   = load_model_dsa(model_file_path, model_uri )
+        return self.pack(sklearn_model)
+
+    def get(self):
+        return self._model
+
+    def save(self, dst, stats=None):        
+        self._model.save(model_path, stats)        
+
+
+    def set_dependencies(self, env):
+        env.add_pip_packages(['scikit-learn'])
+
+
+
 if model_frameowrk == 'sklearn' :
     #A minimum prediction service exposing a Scikit-learn model
     @env(infer_pip_packages=False)
