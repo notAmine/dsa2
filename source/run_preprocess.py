baf194b5a2833a7dd94e40b418af3dcbff32cb3a
arita37 <noelkev0@gmail.com>
2021-03-05 22:03:01 +0900

    test_features.py
    Former-commit-id: 4c83bda78d177285593d94528429d5e243cb9187
    Former-commit-id: 6e6863621cae77a76c531e8aa0e8d0296a10e248

--- source/run_preprocess.py
+++ source/run_preprocess.py
@@ -188,10 +188,10 @@ def preprocess(path_train_X="", path_train_y="", path_pipeline_export="", cols_g
            pars['colid']           = colid
            pars['colcross_single'] = cols_group.get('colcross', [])
 
-       elif col_type == 'add_coly':
-           log( 'add_coly genetic', cols_group['coly'] )
-           pars['coly'] = cols_group['coly']
-           pars['dfy']  = dfi_all[ 'coly' ]  ### Transformed dfy
+       #elif col_type == 'add_coly':
+       #    log( 'add_coly genetic', cols_group['coly'] )
+       pars['coly'] = cols_group['coly']
+       pars['dfy']  = dfi_all[ 'coly' ]  ### add dfy by default
 
        ### Input columns or prevously Computed Columns ( colnum_bin )
        cols_list  = cols_group[cols_name] if cols_name in cols_group else list(dfi_all[cols_name].columns)
@@ -199,7 +199,7 @@ def preprocess(path_train_X="", path_train_y="", path_pipeline_export="", cols_g
        #cols_list  = list(dfi_all[cols_name].columns)
        #df_        = dfi_all[cols_name]
 
-       dfi, col_pars = pipe_fun(df, cols_list, pars= pars)
+       dfi, col_pars = pipe_fun(df_, cols_list, pars= pars)
 
 
        ### Concatenate colnum, colnum_bin into cols_family_all , dfi_all  ###########################
