6f35fa63359837fc36f8c32b3cb82dccb1c3f21e
arita37 <noelkev0@gmail.com>
2021-03-07 23:31:41 +0900

    zold_prepro_sampler.py

--- source/models/keras_widedeep.py
+++ source/models/keras_widedeep.py
@@ -4,6 +4,8 @@
 python source/models/keras_widedeep.py  test
 
 
+python keras_widedeep.py  test
+
 pip install Keras==2.4.3
 
 
@@ -29,7 +31,7 @@ def log(*s):
 
 
 ####################################################################################################
-global model, session, cols_input_formodel
+global model, session
 
 
 
@@ -38,6 +40,8 @@ def init(*kw, **kwargs):
     model = Model(*kw, **kwargs)
     session = None
 
+
+cols_input_formodel = ['cols_cross_input', 'cols_deep_input', 'cols_deep_input' ]
 
 def Modelcustom(n_wide_cross, n_wide, n_feat=8, m_EMBEDDING=10, loss='mse', metric = 'mean_squared_error'):
 
@@ -76,6 +80,65 @@ def Modelcustom(n_wide_cross, n_wide, n_feat=8, m_EMBEDDING=10, loss='mse', metr
         return model
 
 
+def get_dataset_tuple(Xtrain, cols_type, cols_input_formodel ):
+    """  Split into Tuples to feed Keras
+    :param Xtrain:
+    :param cols_type:
+    :param cols_input_formodel:
+    :return:
+    """
+    Xtuple_train = []
+    for cols_groupname in cols_input_formodel :
+        cols_i = cols_type[cols_groupname]
+        Xtuple_train.append( Xtrain[cols_i] )
+
+    return Xtuple_train
+
+
+def get_dataset(data_pars=None, task_type="train", **kw):
+    """
+      return tuple of dataframes
+    """
+    # log(data_pars)
+    data_type = data_pars.get('type', 'ram')
+
+    if data_type == "ram":
+        cols_input_formodel = ['cols_cross_input', 'cols_deep_input', 'cols_deep_input' ]
+        ### dict  colgroup ---> list of colname
+        cols_type     = data_pars['cols_model_type2']  ##3 Sparse, Continuous
+
+        if task_type == "predict":
+            d = data_pars[task_type]
+            Xtrain       = d["X"]
+            Xtuple_train = get_dataset_tuple(Xtrain, cols_type, cols_input_formodel )
+            return Xtuple_train
+
+
+        if task_type == "eval":
+            d = data_pars[task_type]
+            Xtrain, ytrain  = d["X"], d["y"]
+            Xtuple_train    =  get_dataset_tuple(Xtrain, cols_type, cols_input_formodel )
+            return Xtuple_train, ytrain
+
+
+        if task_type == "train":
+            d = data_pars[task_type]
+            Xtrain, ytrain, Xtest, ytest  = d["Xtrain"], d["ytrain"], d["Xtest"], d["ytest"]
+
+            ### dict  colgroup ---> list of df
+            Xtuple_train = get_dataset_tuple(Xtrain, cols_type, cols_input_formodel )
+            Xtuple_test  = get_dataset_tuple(Xtest, cols_type, cols_input_formodel )
+
+            return Xtuple_train, ytrain, Xtuple_test, ytest
+
+
+    elif data_type == "file":
+        raise Exception(f' {data_type} data_type Not implemented ')
+
+    raise Exception(f' Requires  Xtrain", "Xtest", "ytrain", "ytest" ')
+
+
+
 class Model(object):
     def __init__(self, model_pars=None, data_pars=None, compute_pars=None):
         self.model_pars, self.compute_pars, self.data_pars = model_pars, compute_pars, data_pars
@@ -84,12 +147,13 @@ class Model(object):
             self.model = None
         else:
             model_class = model_pars['model_class']  # globals() removed
-            
-            model_pars = {
-                'n_wide_cross' : len(data_pars['cols_model_type2']['cols_wide_input']),
-                'n_wide' : len(data_pars['cols_model_type2']['cols_deep_input']),
-            }
-            self.model  = Modelcustom(**model_pars)
+
+            ### Dynamic sizing
+            model_pars['model_pars']['n_wide_cross'] = len(data_pars['cols_model_type2']['cols_cross_input'])
+            model_pars['model_pars']['n_wide']       = len(data_pars['cols_model_type2']['cols_deep_input'])
+            mdict = model_pars['model_pars']
+
+            self.model  = Modelcustom(**mdict)
             if VERBOSE: log(model_class, self.model)
             self.model.summary()
 
@@ -104,6 +168,14 @@ def fit(data_pars=None, compute_pars=None, out_pars=None, **kw):
 
     cpars = compute_pars.get("compute_pars", {})
     assert 'epochs' in cpars, 'epoch'
+
+    out_pars ={} if out_pars is None else out_pars
+    early_stopping = EarlyStopping(monitor='loss', patience=3)
+    model_ckpt     = ModelCheckpoint(filepath = out_pars.get('path_ckpt', '/home/hari/model_.pth'),
+                                     save_best_only=True, monitor='loss')
+    callbacks      = [early_stopping, model_ckpt]
+    cpars['callbacks'] =  callbacks
+
 
     hist = model.model.fit( Xtrain_tuple, ytrain,  **cpars)
     model.history = hist
@@ -217,71 +289,9 @@ def preprocess(prepro_pars):
         return None, None, Xtest, ytest
 
 
+
 ####################################################################################################
 ############ Do not change #########################################################################
-def get_dataset(data_pars=None, task_type="train", **kw):
-    """
-      return tuple of dataframes
-    """
-    # log(data_pars)
-    data_type = data_pars.get('type', 'ram')
-
-    if data_type == "ram":
-        cols_input_formodel = ['cols_wide_input', 'cols_deep_input', 'cols_deep_input' ]
-        ### dict  colgroup ---> list of colname
-        cols_type     = data_pars['cols_model_type2']
-
-        if task_type == "predict":
-            d = data_pars[task_type]
-            Xtrain       = d["X"]
-
-            Xtuple_train = []
-            for cols_groupname in cols_input_formodel :
-                cols_i = cols_type[cols_groupname]
-                Xtuple_train.append( Xtrain[cols_i] )
-
-            return Xtuple_train
-
-
-        if task_type == "eval":
-            d = data_pars[task_type]
-            Xtrain, ytrain  = d["X"], d["y"]
-
-            Xtuple_train = []
-            for cols_groupname in cols_input_formodel :
-                cols_i = cols_type[cols_groupname]
-                Xtuple_train.append( Xtrain[cols_i] )
-
-            return Xtuple_train, ytrain
-
-
-        if task_type == "train":
-            d = data_pars[task_type]
-            Xtrain, ytrain, Xtest, ytest  = d["Xtrain"], d["ytrain"], d["Xtest"], d["ytest"]
-
-            cols_input_formodel = ['cols_wide_input', 'cols_deep_input', 'cols_deep_input' ]
-            cols_type           = data_pars['cols_model_type2']   ##3 Sparse, Continuous
-            ### dict  colgroup ---> list of df
-            Xtuple_train = []
-            for cols_groupname in cols_input_formodel :
-                cols_i = cols_type[cols_groupname]
-                Xtuple_train.append( np.array(Xtrain[cols_i] ))
-
-            Xtuple_test = []
-            for cols_groupname in cols_input_formodel :
-                cols_i = cols_type[cols_groupname]
-                Xtuple_test.append( np.array(Xtest[cols_i] ))
-
-            return Xtuple_train, ytrain, Xtuple_test, ytest
-
-
-    elif data_type == "file":
-        raise Exception(f' {data_type} data_type Not implemented ')
-
-    raise Exception(f' Requires  Xtrain", "Xtest", "ytrain", "ytest" ')
-
-
-
 def test(config=''):
     """
         Group of columns for the input model
@@ -296,25 +306,21 @@ def test(config=''):
 
     X = pd.DataFrame( np.random.rand(100,30), columns= [ 'col_' +str(i) for i in range(30)] )
     y = pd.DataFrame( np.random.binomial(n=1, p=0.5, size=[100]), columns = ['coly'] )
-
-
     X_train_full, X_test, y_train_full, y_test = train_test_split(X, y, random_state=2021, stratify=y)
     X_train, X_valid, y_train, y_valid         = train_test_split(X_train_full, y_train_full, random_state=2021, stratify=y_train_full)
 
+    #### Call back
     early_stopping = EarlyStopping(monitor='loss', patience=3)
     model_ckpt     = ModelCheckpoint(filepath='/home/hari/model_.pth', save_best_only=True, monitor='loss')
     callbacks      = [early_stopping, model_ckpt]
 
-    n_features      = X_train.shape[1]  # number of features
-    n_wide_features = 20
-    n_deep_features = n_features - n_wide_features
+    # n_features      = X_train.shape[1]  # number of features
+    # n_wide_features = 20
+    # n_deep_features = n_features - n_wide_features
 
 
     #############################################################
     ##### Generate column actual names from
-    # colnum = [ 'col_0', 'col_1']
-    # colcat = [ 'col_2', 'col_3']
-
     colnum = [ 'col_0', 'col_11', 'col_8']
     colcat = [ 'col_13', 'col_17', 'col_13', 'col_9']
 
@@ -325,7 +331,7 @@ def test(config=''):
 
     ###### Keras has 1 tuple input    ###########################
     colg_input = {
-      'cols_wide_input':   ['colnum', 'colcat' ],
+      'cols_cross_input':  ['colnum', 'colcat' ],
       'cols_deep_input':   ['colnum', 'colcat' ],
     }
 
@@ -337,11 +343,6 @@ def test(config=''):
 
 
     ##################################################################################
-    # model_pars = {'model_class': 'WideAndDeep',
-    #               'model_pars': {'n_wide_cross': n_wide_features,
-    #                              'n_wide':       n_deep_features},
-    #              }
-
     model_pars = {'model_class': 'WideAndDeep',
                   'model_pars': {},
                 }
@@ -352,8 +353,6 @@ def test(config=''):
 
                   'cols_model_group': ['colnum',
                                        'colcat',
-                                       # 'coltext',
-                                       # 'coldate',
                                        # 'colcross_pair'
                                        ],
 
@@ -364,10 +363,8 @@ def test(config=''):
         , 'filter_pars': {'ymax': 2, 'ymin': -1}
                   }
 
-    data_pars['train'] ={'Xtrain': X_train,
-                           'ytrain': y_train,
-                           'Xtest': X_test,
-                           'ytest': y_test}
+    data_pars['train'] ={'Xtrain': X_train,  'ytrain': y_train,
+                         'Xtest': X_test,  'ytest': y_test}
     data_pars['eval'] =  {'X': X_valid,
                           'y': y_valid}
     data_pars['predict'] = {'X': X_valid}
@@ -377,6 +374,147 @@ def test(config=''):
 
     ######## Run ###########################################
     test_helper(model_pars, data_pars, compute_pars)
+
+
+
+
+def get_params_sklearn(deep=False):
+    return model.model.get_params(deep=deep)
+
+
+def get_params(param_pars={}, **kw):
+    import json
+    # from jsoncomment import JsonComment ; json = JsonComment()
+    pp = param_pars
+    choice = pp['choice']
+    config_mode = pp['config_mode']
+    data_path = pp['data_path']
+
+    if choice == "json":
+        cf = json.load(open(data_path, mode='r'))
+        cf = cf[config_mode]
+        return cf['model_pars'], cf['data_pars'], cf['compute_pars'], cf['out_pars']
+
+    else:
+        raise Exception(f"Not support choice {choice} yet")
+
+
+def test_helper(model_pars, data_pars, compute_pars):
+    global model, session
+
+    root  = "ztmp/"
+    model = Model(model_pars=model_pars, data_pars=data_pars, compute_pars=compute_pars)
+
+    log('\n\nTraining the model..')
+    fit(data_pars=data_pars, compute_pars=compute_pars, out_pars=None)
+
+
+    log('Predict data..')
+    ypred, ypred_proba = predict(Xpred=None, data_pars=data_pars, compute_pars=compute_pars)
+    log(f'Top 5 y_pred: {np.squeeze(ypred)[:5]}')
+
+
+    log('Evaluating the model..')
+    log(eval(data_pars=data_pars, compute_pars=compute_pars))
+    #
+    log('Saving model..')
+    save(path= root + '/model_dir/')
+
+    log('Load model..')
+    model, session = load_model(path= root + "/model_dir/")
+    log('Model successfully loaded!\n\n')
+
+    log('Model architecture:')
+    log(model.summary())
+
+
+#######################################################################################
+if __name__ == "__main__":
+    import fire
+    fire.Fire(test)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def test2(config=''):
+
+    global model, session
+
+    X = np.random.rand(100,30)
+    y = np.random.binomial(n=1, p=0.5, size=[100])
+
+    X_train_full, X_test, y_train_full, y_test = train_test_split(X, y, random_state=2021, stratify=y)
+    X_train, X_valid, y_train, y_valid         = train_test_split(X_train_full, y_train_full, random_state=2021, stratify=y_train_full)
+
+    early_stopping = EarlyStopping(monitor='loss', patience=3)
+    model_ckpt     = ModelCheckpoint(filepath='', save_best_only=True, monitor='loss')
+    callbacks      = [early_stopping, model_ckpt]
+
+    n_features      = X_train.shape[1]  # number of features
+    n_wide_features = 20
+    n_deep_features = n_features - n_wide_features
+
+
+    n_wide = calc(data_pars)
+    n_deep = calc( data_pars)
+
+    self.model = Modelcustom(**model_pars['model_pars'])
+    if VERBOSE: log(model_class, self.model)
+    self.model.summary()
+
+
+    model_pars = {'model_class': 'WideAndDeep',
+                  'model_pars': {'n_wide_cross': n_wide_features,
+                                 'n_wide':       n_deep_features},
+                 }
+    data_pars = {'train': {'Xtrain': X_train,
+                           'ytrain': y_train,
+                           'Xtest': X_test,
+                           'ytest': y_test},
+                 'eval': {'X': X_valid,
+                          'y': y_valid},
+                 'predict': {'X': X_valid},
+                 'n_features': n_features,
+                 'n_wide_features': n_wide_features,
+                 'n_deep_features': n_deep_features,
+                }
+    compute_pars = { 'compute_pars' : { 'epochs': 2,
+                    'callbacks': callbacks} }
+
+    test_helper(model_pars, data_pars, compute_pars)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def get_dataset2(data_pars=None, task_type="train", **kw):
     """
@@ -436,112 +574,3 @@ def get_dataset2(data_pars=None, task_type="train", **kw):
         raise Exception(f' {data_type} data_type Not implemented ')
 
     raise Exception(f' Requires  Xtrain", "Xtest", "ytrain", "ytest" ')
-
-def get_params_sklearn(deep=False):
-    return model.model.get_params(deep=deep)
-
-
-def get_params(param_pars={}, **kw):
-    import json
-    # from jsoncomment import JsonComment ; json = JsonComment()
-    pp = param_pars
-    choice = pp['choice']
-    config_mode = pp['config_mode']
-    data_path = pp['data_path']
-
-    if choice == "json":
-        cf = json.load(open(data_path, mode='r'))
-        cf = cf[config_mode]
-        return cf['model_pars'], cf['data_pars'], cf['compute_pars'], cf['out_pars']
-
-    else:
-        raise Exception(f"Not support choice {choice} yet")
-
-
-def test_helper(model_pars, data_pars, compute_pars):
-    global model, session
-
-    root  = "ztmp/"
-    model = Model(model_pars=model_pars, data_pars=data_pars, compute_pars=compute_pars)
-
-    log('\n\nTraining the model..')
-    fit(data_pars=data_pars, compute_pars=compute_pars, out_pars=None)
-    log('Training completed!\n\n')
-
-    log('Predict data..')
-    ypred, ypred_proba = predict(Xpred=None, data_pars=data_pars, compute_pars=compute_pars)
-    log(f'Top 5 y_pred: {np.squeeze(ypred)[:5]}')
-    log('Data successfully predicted!\n\n')
-
-    log('Evaluating the model..')
-    log(eval(data_pars=data_pars, compute_pars=compute_pars))
-    log('Evaluating completed!\n\n')
-    #
-    log('Saving model..')
-    save(path= root + '/model_dir/')
-    log('Model successfully saved!\n\n')
-
-    log('Load model..')
-    model, session = load_model(path= root + "/model_dir/")
-    log('Model successfully loaded!\n\n')
-
-    log('Model architecture:')
-    log(model.summary())
-
-
-#######################################################################################
-
-def test2(config=''):
-
-    global model, session
-
-    X = np.random.rand(100,30)
-    y = np.random.binomial(n=1, p=0.5, size=[100])
-
-    X_train_full, X_test, y_train_full, y_test = train_test_split(X, y, random_state=2021, stratify=y)
-    X_train, X_valid, y_train, y_valid         = train_test_split(X_train_full, y_train_full, random_state=2021, stratify=y_train_full)
-
-    early_stopping = EarlyStopping(monitor='loss', patience=3)
-    model_ckpt     = ModelCheckpoint(filepath='', save_best_only=True, monitor='loss')
-    callbacks      = [early_stopping, model_ckpt]
-
-    n_features      = X_train.shape[1]  # number of features
-    n_wide_features = 20
-    n_deep_features = n_features - n_wide_features
-
-
-    n_wide = calc(data_pars)
-    n_deep = calc( data_pars)
-
-    self.model = Modelcustom(**model_pars['model_pars'])
-    if VERBOSE: log(model_class, self.model)
-    self.model.summary()
-
-
-    model_pars = {'model_class': 'WideAndDeep',
-                  'model_pars': {'n_wide_cross': n_wide_features,
-                                 'n_wide':       n_deep_features},
-                 }
-    data_pars = {'train': {'Xtrain': X_train,
-                           'ytrain': y_train,
-                           'Xtest': X_test,
-                           'ytest': y_test},
-                 'eval': {'X': X_valid,
-                          'y': y_valid},
-                 'predict': {'X': X_valid},
-                 'n_features': n_features,
-                 'n_wide_features': n_wide_features,
-                 'n_deep_features': n_deep_features,
-                }
-    compute_pars = { 'compute_pars' : { 'epochs': 2,
-                    'callbacks': callbacks} }
-
-    test_helper(model_pars, data_pars, compute_pars)
-
-
-
-
-
-if __name__ == "__main__":
-    import fire
-    fire.Fire(test)
