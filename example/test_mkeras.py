2a3ee72c51f7963ec4e91a7d299486cb1b9c91eb
arita37 <noelkev0@gmail.com>
2021-03-08 20:04:19 +0900

    keras is fixed
    Former-commit-id: cf74713503d49ea8be3234655fa1668e384491bd
    Former-commit-id: 831ef7291ba0faa3e0a268e77e3cf4210d517c80

--- example/test_mkeras.py
+++ example/test_mkeras.py
@@ -3,9 +3,9 @@
 """
 
   python example/mkeras.py  train    --config config1
-
   python mkeras.py  predict  > zlog/log_titanic_predict.txt 2>&1
 
+  Bug on dimenson
 
 """
 import warnings, copy, os, sys
