5daf99b9750591ba1de765a1a648d0e15fbd85d8
Ahmed3bbas <eng.ahmedkhaled53@gmail.com>
2021-03-12 10:41:48 +0200

    run SDV models
    the main issue is get_dataset_tuple function.

--- source/models/model_sampler.py
+++ source/models/model_sampler.py
@@ -30,8 +30,10 @@ sys.path.append( os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "
 
 
 import os, pandas as pd, numpy as np,  sklearn
+import copy
 from sklearn.cluster import KMeans, DBSCAN, Birch
 from sklearn.decomposition import TruncatedSVD, MiniBatchSparsePCA, FastICA
+
 from umap import UMAP
 
 try:
@@ -51,6 +53,10 @@ except:
     from sdv.evaluation import evaluate  
 
 ### IMBLEARN
+import six
+import sys
+sys.modules['sklearn.externals.six'] = six
+
 from imblearn.over_sampling import SMOTE
 from imblearn.combine import SMOTEENN, SMOTETomek
 from imblearn.under_sampling import NearMiss
@@ -63,6 +69,10 @@ try:
 except:
     os.system("pip install mmae[keras]")
 ####################################################################################################
+
+# CONSTANTS
+SDV_MODLES = ['TVAE', 'CTGAN', 'PAR'] # The Synthetic Data Vault Models
+IMBLEARN_MODELS = ['SMOTE', 'SMOTEENN', 'SMOTETomek', 'NearMiss']
 verbosity = 3
 
 def log(*s):
@@ -255,8 +265,7 @@ def fit(data_pars: dict=None, compute_pars: dict=None, out_pars: dict=None, **kw
 
     cpars = copy.deepcopy(compute_pars.get("compute_pars", {}))
 
-    list_unsupervised = [  'TVAE', 'CTGAN', 'PAR'  ]
-    if ytrain is not None and model.model_pars['model_class'] not in list_unsupervised :  ###with label
+    if ytrain is not None and model.model_pars['model_class'] not in SDV_MODLES :  ###with label
        model.model.fit(Xtrain_tuple, ytrain, **cpars)
     else :
        model.model.fit(Xtrain_tuple, **cpars)
@@ -271,12 +280,15 @@ def eval(data_pars=None, compute_pars=None, out_pars=None, **kw):
 
     data_pars['train'] = True
     Xval, yval         = get_dataset(data_pars, task_type="eval")
-
     Xnew               = transform(Xval, data_pars, compute_pars, out_pars)
     # log(data_pars)
     mpars = compute_pars.get("metrics_pars", {'aggregate': True})
-    evals = evaluate(Xnew, Xval, **mpars )
-    return evals
+
+    if model.model_pars['model_class'] in SDV_MODLES:
+        evals = evaluate(Xnew, Xval, **mpars )
+        return evals
+    else:
+        return None
 
 
 def transform(Xpred=None, data_pars={}, compute_pars={}, out_pars={}, **kw):
@@ -288,6 +300,7 @@ def transform(Xpred=None, data_pars={}, compute_pars={}, out_pars={}, **kw):
     :param kw:
     :return:
     """
+
     global model, session
     post_process_fun = model.model_pars.get('post_process_fun', None)
     if post_process_fun is None:
@@ -298,19 +311,19 @@ def transform(Xpred=None, data_pars={}, compute_pars={}, out_pars={}, **kw):
     if Xpred is None:
         Xpred_tuple = get_dataset(data_pars, task_type="predict")
     else :
-        cols_type         = data_pars['cols_model_type2']
-        cols_ref_formodel = cols_type
-        Xpred_tuple       = get_dataset_tuple(Xpred, cols_type, cols_ref_formodel)
+        # cols_type         = data_pars['cols_model_type2']
+        # cols_ref_formodel = cols_type
+        Xpred_tuple       = Xpred # get_dataset_tuple(Xpred, cols_type, cols_ref_formodel)
 
     Xnew= None
-    if model.model_pars['model_class'] in ['CTGAN', 'TVAE', 'PAR'] :
+    if model.model_pars['model_class'] in SDV_MODLES :
        Xnew = model.model.sample(compute_pars.get('n_sample_generation', 100) )
 
     elif model.model_pars['model_class'] in ['SMOTE', ] :   ### Sampler
-       Xnew = model.model.resample( Xpred, **compute_pars.get('compute_pars', {}) )
+       Xnew = model.model.resample( Xpred_tuple, **compute_pars.get('compute_pars', {}) )
 
     else :
-       Xnew = model.model.transform( Xpred, **compute_pars.get('compute_pars', {}) )
+       Xnew = model.model.transform( Xpred_tuple, **compute_pars.get('compute_pars', {}) )
 
     log3("generated data", Xnew)
     return Xnew
@@ -377,7 +390,7 @@ def get_dataset_tuple(Xtrain, cols_type_received, cols_ref):
     """
     if len(cols_ref) < 1 :
         return Xtrain
-
+    
     Xtuple_train = []
     for cols_groupname in cols_ref :
         assert cols_groupname in cols_type_received, "Error missing colgroup in config data_pars[cols_model_type] "
@@ -409,26 +422,28 @@ def get_dataset(data_pars=None, task_type="train", **kw):
         if task_type == "predict":
             d = data_pars[task_type]
             Xtrain       = d["X"]
-            Xtuple_train = get_dataset_tuple(Xtrain, cols_type_received, cols_ref)
-            return Xtuple_train
+            # Xtuple_train = get_dataset_tuple(Xtrain, cols_type_received, cols_ref)
+            return Xtrain # Xtuple_train
 
         if task_type == "eval":
             d = data_pars[task_type]
             Xtrain, ytrain  = d["X"], d["y"]
-            Xtuple_train    = get_dataset_tuple(Xtrain, cols_type_received, cols_ref)
-            return Xtuple_train, ytrain
+            # Xtuple_train    = get_datas1et_tuple(Xtrain, cols_type_received, cols_ref)
+            # return Xtuple_train, ytrain
+            return Xtrain, ytrain
 
         if task_type == "train":
             d = data_pars[task_type]
             Xtrain, ytrain, Xtest, ytest  = d["Xtrain"], d["ytrain"], d["Xtest"], d["ytest"]
 
             ### dict  colgroup ---> list of df
-            Xtuple_train = get_dataset_tuple(Xtrain, cols_type_received, cols_ref)
-            Xtuple_test  = get_dataset_tuple(Xtest, cols_type_received, cols_ref)
+            # Xtuple_train = get_dataset_tuple(Xtrain, cols_type_received, cols_ref)
+            # Xtuple_test  = get_dataset_tuple(Xtest, cols_type_received, cols_ref)
 
-            log2("Xtuple_train", Xtuple_train)
+            # log2("Xtuple_train", Xtuple_train)
 
-            return Xtuple_train, ytrain, Xtuple_test, ytest
+            # return Xtuple_train, ytrain, Xtuple_test, ytest
+            return Xtrain, ytrain, Xtest, ytest
 
     elif data_type == "file":
         raise Exception(f' {data_type} data_type Not implemented ')
@@ -469,7 +484,7 @@ def test():
     for colg, colist in colg_input.items() :
         cols_model_type2[colg] = []
         for colg_i in colist :
-          cols_model_type2[colg].extend( cols_input_type_1[colg_i] )
+          cols_model_type2[colg].extend( [i for i in cols_input_type_1[colg_i] if i not in y.columns ]   )
     ###############################################################################
     n_sample = 100
     data_pars = {'n_sample': n_sample,
@@ -493,17 +508,19 @@ def test():
                           'y': y_valid}
     data_pars['predict'] = {'X': X_valid}
 
-    compute_pars = { 'compute_pars' : { 'epochs': 2,
+    compute_pars = { 'compute_pars' : { # 'epochs': 2,
                    } }
 
     #####################################################################
-    log("test 1")
-    model_pars = {'model_class': 'TruncatedSVD',
-                  'model_pars': {
-                     # 'ratio' :'auto',
-                },
-                }
-    test_helper(model_pars, data_pars, compute_pars)
+    # log("test 1")
+    # model_pars = {'model_class': 'TruncatedSVD',
+    #               'model_pars': {
+    #                   "n_components": 3,
+    #                   'n_iter': 2,
+    #                  # 'ratio' :'auto',
+    #             },
+    #             }
+    # test_helper(model_pars, data_pars, compute_pars)
 
 
 
@@ -539,7 +556,7 @@ def test():
 
 
 
-    log("test 2")
+    log("test 2 --> CTGAN")
     model_pars = {'model_class': 'CTGAN',
                   'model_pars': {
                      ## CTGAN
@@ -550,7 +567,43 @@ def test():
                      'discriminator_dim' : (256, 256, 256)
                 },
                 }
+    compute_pars = { 
+        'compute_pars' : {}
+        }
     test_helper(model_pars, data_pars, compute_pars)
+
+
+    # log("test 3 --> TVAE")
+    # model_pars = {'model_class': 'TVAE',
+    #               'model_pars': { 
+    #                   ## TVAE
+    #                  'primary_key': colid,
+    #                  'epochs': 1,
+    #                  'batch_size' :100,
+    #             },
+    #             }
+    # compute_pars = { 
+    #     'compute_pars' : {}
+    #     }
+    # test_helper(model_pars, data_pars, compute_pars)
+
+    # log("test 4 --> PAR")
+    # entity_columns = [colid]
+    # context_columns = None
+    # sequence_index = None
+    # model_pars = {'model_class': 'PAR',
+    #               'model_pars': {
+    #                  ## PAR
+    #                  'epochs': 1,
+    #                  'entity_columns': entity_columns,
+    #                  'context_columns': context_columns,
+    #                  'sequence_index': sequence_index
+    #             },
+    #             }
+    # compute_pars = { 
+    #     'compute_pars' : {}
+    #     }
+    # test_helper(model_pars, data_pars, compute_pars)
 
 
 def test_helper(model_pars, data_pars, compute_pars):
