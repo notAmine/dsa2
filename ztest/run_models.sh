af47bc6c7f81d7b7720289bcf4de5f4198d41380
arita37 <noelkev0@gmail.com>
2021-03-20 11:29:34 +0900

    model testing

---
+++ ztest/run_models.sh
@@ -0,0 +1,21 @@
+
+
+pwd
+which python
+ls .
+cd source/models
+
+
+python torch_tabular.py test
+
+
+python model_vaemdn.py test
+
+
+python model_bayesian_pyro.py test
+
+
+python keras_widedeep.py test
+
+
+
