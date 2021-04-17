f4730c7973fd6affed118745442407c1c42da228
arita37 <noelkev0@gmail.com>
2021-04-17 22:52:19 +0900

    test_hyperopt.py

--- example/test_mkeras.py
+++ example/test_mkeras.py
@@ -200,6 +200,10 @@ from core_run import predict
 ###########################################################################################################
 ###########################################################################################################
 if __name__ == "__main__":
-    d = { "train" : train, "predict" : predict, "config" : config_default }
+    from pyinstrument import Profiler;  profiler = Profiler() ; profiler.start()
     import fire
-    fire.Fire(d)
+    fire.Fire()
+    profiler.stop() ; print(profiler.output_text(unicode=True, color=True))
+
+
+
